7d6d3fa0ec631aea1e6a81e18b52676559c8d66c
Leevi Uotinen <uotinen.leevi@hotmail.com>
2022-09-02 17:45:20 +0300

    Refactor math functions.

--- src/include/uo_math.h
+++ src/include/uo_math.h
@@ -15,16 +15,6 @@ extern "C"
 #define uo_avx_float __m256
 #define uo_floats_per_avx_float (sizeof(uo_avx_float) / sizeof(float))
 
-  typedef struct uo_matrix
-  {
-    struct
-    {
-      size_t rows;
-      size_t cols;
-    } size;
-    uo_avx_float *data;
-  } uo_matrix;
-
   typedef uo_avx_float uo_mm256_ps_function(uo_avx_float avx_float);
 
   static inline size_t uo_mm256_roundup_size(size_t size)
@@ -43,132 +33,76 @@ extern "C"
     return _mm_cvtss_f32(sums);
   }
 
-  static inline float uo_mm256_dotproduct_ps(uo_avx_float *a, uo_avx_float *b, size_t n)
+  static inline float uo_dotproduct_ps(const float *a, const float *b, size_t n)
   {
     size_t block_count = n / uo_floats_per_avx_float;
     uo_avx_float sum = _mm256_setzero_ps();
 
     for (size_t i = 0; i < block_count; ++i)
     {
-      uo_avx_float mul = _mm256_mul_ps(a[i], b[i]);
+      uo_avx_float _a = _mm256_loadu_ps(a + i * uo_floats_per_avx_float);
+      uo_avx_float _b = _mm256_loadu_ps(b + i * uo_floats_per_avx_float);
+      uo_avx_float mul = _mm256_mul_ps(_a, _b);
       sum = _mm256_add_ps(sum, mul);
     }
 
     return uo_mm256_hsum_ps(sum);
   }
 
-  static inline void uo_mm256_load_ps(uo_avx_float *dst, float *src, size_t n)
-  {
-    size_t block_count = n / uo_floats_per_avx_float;
-
-    for (size_t i = 0; i < block_count; ++i)
-    {
-      dst[i] = _mm256_loadu_ps(src + i * uo_floats_per_avx_float);
-    }
-  }
-
-  static inline void uo_mm256_load_transpose_ps(uo_avx_float *dst, float *src, size_t n, size_t size_cols)
-  {
-    size_t block_count = n / uo_floats_per_avx_float;
-    __m256i vindex = _mm256_set_epi32(
-      0 * size_cols,
-      1 * size_cols,
-      2 * size_cols,
-      3 * size_cols,
-      4 * size_cols,
-      5 * size_cols,
-      6 * size_cols,
-      7 * size_cols
-    );
-
-    for (size_t i = 0; i < block_count; ++i)
-    {
-      dst[i] = _mm256_i32gather_ps(src + i * uo_floats_per_avx_float, vindex, sizeof(float));
-    }
-  }
-
-  static inline void uo_mm256_store_ps(float *dst, uo_avx_float *src, size_t n)
-  {
-    size_t block_count = n / uo_floats_per_avx_float;
-
-    for (size_t i = 0; i < block_count; ++i)
-    {
-      _mm256_storeu_ps(dst + i * uo_floats_per_avx_float, src[i]);
-    }
-  }
-
-  static inline void uo_mm256_vecadd_ps(uo_avx_float *a, uo_avx_float *b, uo_avx_float *c, size_t n)
+  static inline void uo_add_ps(float *a, float *b, float *c, size_t n)
   {
     size_t block_count = n / uo_floats_per_avx_float;
     uo_avx_float sum = _mm256_setzero_ps();
 
     for (size_t i = 0; i < block_count; ++i)
     {
-      c[i] = _mm256_add_ps(a[i], b[i]);
+      uo_avx_float _a = _mm256_loadu_ps(a + i * uo_floats_per_avx_float);
+      uo_avx_float _b = _mm256_loadu_ps(b + i * uo_floats_per_avx_float);
+      uo_avx_float add = _mm256_add_ps(_a, _b);
+      _mm256_store_ps(c + i * uo_floats_per_avx_float, add);
     }
   }
 
-  static inline void uo_mm256_vecsub_ps(uo_avx_float *a, uo_avx_float *b, uo_avx_float *c, size_t n)
+  static inline void uo_sub_ps(float *a, float *b, float *c, size_t n)
   {
     size_t block_count = n / uo_floats_per_avx_float;
     uo_avx_float sum = _mm256_setzero_ps();
 
     for (size_t i = 0; i < block_count; ++i)
     {
-      c[i] = _mm256_sub_ps(a[i], b[i]);
+      uo_avx_float _a = _mm256_loadu_ps(a + i * uo_floats_per_avx_float);
+      uo_avx_float _b = _mm256_loadu_ps(b + i * uo_floats_per_avx_float);
+      uo_avx_float sub = _mm256_sub_ps(_a, _b);
+      _mm256_store_ps(c + i * uo_floats_per_avx_float, sub);
     }
   }
 
-  static inline void uo_mm256_vec_mapfunc_ps(uo_avx_float *v, size_t n, uo_mm256_ps_function *function)
+  static inline void uo__mapfunc_ps(float *v, size_t n, uo_mm256_ps_function *function)
   {
     size_t block_count = n / uo_floats_per_avx_float;
 
     for (size_t i = 0; i < block_count; ++i)
     {
-      v[i] = function(v[i]);
+      uo_avx_float _v = _mm256_loadu_ps(v + i * uo_floats_per_avx_float);
+      uo_avx_float res = function(_v);
+      _mm256_store_ps(v + i * uo_floats_per_avx_float, res);
     }
   }
 
-  static inline void uo_mm256_matmul2_ps(uo_matrix A, uo_matrix Bt, uo_matrix *C)
+  static inline void uo_mm256_matmul_ps(float *A, float *B, float *C, size_t m, size_t n, size_t p)
   {
-    size_t block_count = A.size.cols / uo_floats_per_avx_float;
-    float *r = uo_alloca(Bt.size.rows);
+    size_t block_count = n / uo_floats_per_avx_float;
 
-    C->size.rows = A.size.rows;
-    C->size.cols = Bt.size.rows;
-
-    for (size_t i = 0; i < A.size.rows; ++i)
+    for (size_t i = 0; i < block_count; ++i)
     {
-      uo_avx_float *a = A.data + block_count * i;
-
-      for (size_t j = 0; j < Bt.size.rows; ++j)
+      for (size_t j = 0; j < m; ++j)
       {
-        uo_avx_float *b = Bt.data + block_count * j;
-        r[j] = uo_mm256_dotproduct_ps(a, b, A.size.cols);
-      }
+        uo_avx_float _a = _mm256_loadu_ps(A + j * n + i * uo_floats_per_avx_float);
 
-      for (size_t k = 0; k < block_count; ++k)
-      {
-        C->data[k] = _mm256_loadu_ps(r + k * uo_floats_per_avx_float);
-      }
-    }
-  }
+        for (size_t k = 0; k < p; ++k)
+        {
 
-  static inline void uo_mm256_matmul_ps(uo_avx_float *At, size_t A_m, size_t A_n, uo_avx_float *B, size_t B_n, uo_avx_float *C)
-  {
-    size_t block_count = B_n / uo_floats_per_avx_float;
-    float *r = uo_alloca(B_n);
-
-    for (size_t i = 0; i < A_m; ++i)
-    {
-      for (size_t j = 0; j < B_n; ++j)
-      {
-        r[j] = uo_mm256_dotproduct_ps(At + A_n * i, B + A_n * j, A_n);
-      }
-
-      for (size_t k = 0; k < block_count; ++k)
-      {
-        C[k] = _mm256_loadu_ps(r + k * uo_floats_per_avx_float);
+        }
       }
     }
   }
