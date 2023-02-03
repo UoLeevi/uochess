e430b51c583a6b9f747c59f8c2dac5c105a29809
Leevi Uotinen <uotinen.leevi@hotmail.com>
2023-02-03 18:23:43 +0200

    Write xor nn model file.

--- scripts/model_xor.py
+++ scripts/model_xor.py
@@ -1,35 +1,26 @@
 import tensorflow as tf
 
-x = tf.placeholder(tf.float32, shape=[None, 2], name='input')
-y_true = tf.placeholder(tf.float32, shape=[None, 1], name='target')
+# Define the input layer
+input_layer = tf.keras.layers.Input(shape=(2,))
 
 # Define the first dense layer
-dense_1 = tf.layers.dense(inputs=x, units=64, activation=tf.nn.relu)
+dense_1 = tf.keras.layers.Dense(32, activation='relu')(input_layer)
 
-# Define the dropout layer
-dropout = tf.layers.dropout(inputs=dense_1, rate=0.2)
+# Define the second dense layer
+dense_2 = tf.keras.layers.Dense(32, activation='relu')(dense_1)
 
 # Define the output layer
-y_pred = tf.layers.dense(inputs=dropout, units=10, activation=tf.nn.sigmoid)
+output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(dense_2)
 
+# Define the model
+model = tf.keras.models.Model(inputs=input_layer, outputs=output_layer)
 
-# Optimize loss
-loss = tf.reduce_mean(tf.square(y_pred - y_true), name='loss')
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-train_op = optimizer.minimize(loss, name='train')
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-init = tf.global_variables_initializer()
+# Serialize the model to a string
+model_string = model.to_json()
 
-# tf.train.Saver.__init__ adds operations to the graph to save
-# and restore variables.
-saver_def = tf.train.Saver().as_saver_def()
-
-print('Run this operation to initialize variables     : ', init.name)
-print('Run this operation for a train step            : ', train_op.name)
-print('Feed this tensor to set the checkpoint filename: ', saver_def.filename_tensor_name)
-print('Run this operation to save a checkpoint        : ', saver_def.save_tensor_name)
-print('Run this operation to restore a checkpoint     : ', saver_def.restore_op_name)
-
-# Write the graph out to a file.
-with open('graph_xor.pb', 'w') as f:
-  f.write(tf.get_default_graph().as_graph_def().SerializeToString())
+# Write the model string to a file
+with open("src/nn/model_xor.json", "w") as f:
+    f.write(model_string)
