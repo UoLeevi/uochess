46676bc565dadbba8fa7106e0651049321a14f34
Leevi Uotinen <uotinen.leevi@hotmail.com>
2023-02-10 18:45:45 +0200

    Work on tensorflow integration.

--- scripts/model_xor.py
+++ scripts/model_xor.py
@@ -16,7 +16,16 @@ output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(dense_2)
 model = tf.keras.models.Model(inputs=input_layer, outputs=output_layer)
 
 # Compile the model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+loss_fn = tf.keras.losses.binary_crossentropy
+model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
+
+# Define the target placeholder
+target = tf.keras.Input(shape=(1,), name='target')
+
+# Define the train operation
+with tf.name_scope("train_op"):
+    train_op = optimizer.get_updates(params=model.trainable_variables, loss=loss_fn(model.outputs, target))[0]
 
 # Save the model as a TensorFlow SavedModel
 tf.keras.models.save_model(model, 'src/nn/model_xor')
